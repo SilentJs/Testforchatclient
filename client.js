1eb4687ef07567ad5373cffad1e5d1d461449ea9
SilentJs <86762707+SilentJs@users.noreply.github.com>
2022-04-29 04:40:26 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,34 @@
+const socket = io('https://8d0d-103-252-27-178.in.ngrok.io');
+const messages = document.getElementById('messages');
+const msgForm = document.getElementById('msgForm');
+
+
+
+socket.on('message', data => {
+    console.log(data)
+    appendMessages(data[0],data[1])
+})
+socket.on('output-messages', data => {
+    console.log(data)
+    if (data.length) {
+        data.forEach(message => {
+            appendMessages(message.username,message.msg)
+        });
+    }
+})
+msgForm.addEventListener('submit', e => {
+    e.preventDefault()
+    var array=[msgForm.name.value,msgForm.msg.value]
+    socket.emit('chatmessage', array)
+    console.log('[CLIENT]Username: ', msgForm.name.value) 
+    console.log('[CLIENT]Message: ', msgForm.msg.value)
+    msgForm.msg.value = '';
+    msgForm.name.value = '';
+
+
+})
+
+function appendMessages(name,message) {
+    const html = `<div><li>${name}</li><br><li>${message}</li></div>`
+    messages.innerHTML += html
+}
